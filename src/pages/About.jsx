a883393edf8cd1d4b86c6caa78ff5d5f3feb9066
vinay <thevinay02@gmail.com>
2025-11-15 08:12:51 +0530

    depoly day

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,21 @@ import DeveloperIllustration from '../components/DeveloperIllustration'
 
 const About = () => {
   return (
-    <div className='h-screen flex'>
-      <div className=' h-full  bg-amber-400 w-[60%]'> 
-          <h1>Who AM <i>i</i></h1>           
-         <p> I am S.Vinay , a person who loves to code who learns and builds the things in his way and he learns from it</p>
+    <div className="h-screen grid grid-cols-7 row-span-7">
+      <div className="col-span-4 flex flex-col justify-center items-start px-12 ">
+        <h1 className="underline text-3xl mb-4  ">
+          Who AM <i className="text-[#ffe02f] underline">i</i>
+        </h1>
+        <p>
+          I’m Vinay Sanda.I am currently a student pursuing B.Tech 3<sup>rd</sup> year in the CSBS department at BVRIT.
+        </p>
+        <p>
+
+        </p>
       </div>
-      <div className=' h-full w-[40%] pt-10'><DeveloperIllustration/></div>
+      <div className="col-span-3  ">
+        <DeveloperIllustration className='h-[80%] mt-8'/>
+      </div>
     </div>
   )
 }
